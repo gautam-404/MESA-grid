1e80e7a82b586ca98ed05fdeda620a31ab93a5b7
gautam-404 <gautam.anuj.1195@gmail.com>
2023-02-21 23:21:10 +0530

    update

--- grid.py
+++ grid.py
@@ -55,12 +55,11 @@ def update_live_display(live_disp, progressbar, group, n):
 
 
     
-def evo_star(mass, metallicity, coarse_age, v_surf_init=0, model=0, gyre=False,
+def evo_star(mass, metallicity, v_surf_init=0, model=0, gyre=False,
             save_model=True, logging=True, loadInlists=False, parallel=False, silent=False):
     '''Evolve a star
     Args:   mass (optional, float): mass of star in solar masses
             metallicity (optional, float): metallicity of star
-            coarse_age (optional, float): age of star when we switch to a coarser timestep
             v_surf_init (optional, float): initial surface rotation velocity in km/s
             model (optional, int): model number, used by the code to name the model when parallelizing
             gyre (optional, bool): whether to run gyre after evolution
@@ -86,27 +85,23 @@ def evo_star(mass, metallicity, coarse_age, v_surf_init=0, model=0, gyre=False,
     ## Get Parameters
     terminal_age = float(np.round(2500/initial_mass**2.5,1)*1.0E6)
     phases_params = helper.phases_params(initial_mass, Zinit)     
-    if v_surf_init != 0:
-        templates = sorted(glob.glob("./urot/*inlist*"))
-        if loadInlists:
-            phase_max_age = [1.0E-3, 0.25E6, 1E6, coarse_age, 4.0E7, terminal_age]
-            phases_names = ['Initial Contraction', 'Pre-Main Sequence', 'Start Rotation', 'Hi-Res Evolution', 'Low-Res Evolution', 'Late Main Sequence Evolution']
-        else:
-            phase_max_age = [1.0E-3, 2E6, coarse_age, 4.0E7, terminal_age]
-            phases_names = phases_params.keys()
-        rotation_init_params = {'change_rotation_flag': True,   ## False for rotation off until near zams
-                                'new_rotation_flag': True,
-                                'change_initial_rotation_flag': True,
-                                'set_initial_surface_rotation_v': True,
-                                'set_surface_rotation_v': True,
-                                'new_surface_rotation_v': v_surf_init,
-                                'relax_surface_rotation_v' : True,
-                                'num_steps_to_relax_rotation' : 100, ## Default value is 100
-                                'set_uniform_am_nu_non_rot': True}
+
+    urot_inlists = sorted(glob.glob("./urot/*inlist*"))
+    if loadInlists:
+        phase_max_age = [1.0E-3, 0.25E6, 1E6, 1E7, 4.0E7, terminal_age]
+        phases_names = ['Initial Contraction', 'Pre-Main Sequence', 'Start Rotation', 'Hi-Res Evolution', 'Low-Res Evolution', 'Late Main Sequence Evolution']
     else:
-        templates = sorted(glob.glob("./urot/*inlist*"))
-        phase_max_age = [1.0E-3, 2E6, coarse_age, 4.0E7, terminal_age]
+        phase_max_age = [1.0E-3, 2E6, 1E7, 4.0E7, terminal_age]         ## 1E7 is the age when we switch to a coarser timestep
         phases_names = phases_params.keys()
+    rotation_init_params = {'change_rotation_flag': True,   ## False for rotation off until near zams
+                            'new_rotation_flag': True,
+                            'change_initial_rotation_flag': True,
+                            'set_initial_surface_rotation_v': True,
+                            'set_surface_rotation_v': True,
+                            'new_surface_rotation_v': v_surf_init,
+                            'relax_surface_rotation_v' : True,
+                            'num_steps_to_relax_rotation' : 100, ## Default value is 100
+                            'set_uniform_am_nu_non_rot': True}
 
 
     inlist_template = "./templates/inlist_template"
@@ -114,7 +109,7 @@ def evo_star(mass, metallicity, coarse_age, v_surf_init=0, model=0, gyre=False,
     for phase_name in phases_names:
         try:
             if loadInlists:         ## Run from pre-made inlists
-                star.load_InlistProject(templates.pop(0))
+                star.load_InlistProject(urot_inlists.pop(0))
                 if phase_name == "Start Rotation":
                     continue        ## Skip the start rotation phase, because we started rotation in the contraction phase
             else:                   ## Run from inlist template by setting parameters for each phase
@@ -125,9 +120,8 @@ def evo_star(mass, metallicity, coarse_age, v_surf_init=0, model=0, gyre=False,
             star.set('initial_z', Zinit, force=True)
             star.set('max_age', phase_max_age.pop(0), force=True)
             if phase_name == "Initial Contraction":
-                if v_surf_init != 0:
-                    ## Initiate rotation
-                    star.set(rotation_init_params, force=True)
+                ## Initiate rotation
+                star.set(rotation_init_params, force=True)
                 proj.run(logging=logging, parallel=parallel)
             else:
                 proj.resume(logging=logging, parallel=parallel)
@@ -214,7 +208,7 @@ def run_grid(parallel=False, show_progress=True, testrun=False, create_grid=True
     '''
 
     ## Initialize grid
-    masses, metallicities, coarse_age_list, v_surf_init_list = init_grid(testrun=testrun, create_grid=create_grid)
+    masses, metallicities, v_surf_init_list = init_grid(testrun=testrun, create_grid=create_grid)
 
     ## Create archive directories
     if os.path.exists("grid_archive"):
@@ -253,7 +247,7 @@ def run_grid(parallel=False, show_progress=True, testrun=False, create_grid=True
         n_processes = -(-os.cpu_count() // int(os.environ['OMP_NUM_THREADS']))  ## round up
         # n_processes -= 1   ## leave some breathing room
         length = len(masses)
-        args = zip(masses, metallicities, coarse_age_list, v_surf_init_list,
+        args = zip(masses, metallicities, v_surf_init_list,
                         range(1, length+1), repeat(gyre), repeat(save_model), repeat(logging), 
                         repeat(loadInlists), repeat(parallel), repeat(True))
         if show_progress:
@@ -283,9 +277,9 @@ def run_grid(parallel=False, show_progress=True, testrun=False, create_grid=True
     else:
         # Run grid in serial
         model = 1
-        for mass, metallicity, v_surf_init, coarse_age in zip(masses, metallicities, v_surf_init_list, coarse_age_list):
+        for mass, metallicity, v_surf_init in zip(masses, metallicities, v_surf_init_list):
             print(f"[b i yellow]Running model {model} of {len(masses)}")
-            evo_star(mass, metallicity, coarse_age, v_surf_init, model=model, gyre=gyre,
+            evo_star(mass, metallicity, v_surf_init, model=model, gyre=gyre,
                     save_model=save_model, logging=logging, loadInlists=loadInlists)
             model += 1
             print(f"[b i green]Done with model {model-1} of {len(masses)}")
@@ -303,7 +297,6 @@ def init_grid(testrun=None, create_grid=True):
     Returns:
             masses (list): list of masses
             metallicities (list): list of metallicities
-            coarse_age_list (list): list of coarse ages
             v_surf_init_list (list): list of initial surface velocities
     '''
     def get_grid(sample_masses, sample_metallicities, sample_v_init):
@@ -323,35 +316,32 @@ def init_grid(testrun=None, create_grid=True):
             v_surf_init_list = [2, 4, 6, 8, 10, 12, 14, 16, 18]
             masses = [1.7]*len(v_surf_init_list)
             metallicities = [0.017]*len(v_surf_init_list)
-            coarse_age_list = [1e7]*len(v_surf_init_list)
         if testrun == "grid":
             sample_masses = np.arange(1.30,1.51,0.02)                  ## 1.30 - 1.50 Msun (0.02 Msun step)
             sample_metallicities = np.arange(0.0010,0.0101,0.0010)     ## 0.001 - 0.010 (0.001 step)
             sample_v_init = np.arange(2, 20, 2)                        ## 2 - 18 km/s (2 km/s step)
             masses, metallicities, v_surf_init_list = get_grid(sample_masses, sample_metallicities, sample_v_init)
-            coarse_age_list = 1E7 * np.ones(len(masses)).astype(float)               
+            print(masses, metallicities, v_surf_init_list)            
     else:
         if create_grid:
             ## Create grid
             sample_masses = np.arange(1.36, 2.22, 0.02)                ## 1.36 - 2.20 Msun (0.02 Msun step)
             sample_metallicities = np.arange(0.001, 0.0101, 0.0001)    ## 0.001 - 0.010 (0.0001 step)
-            sample_v_init = np.append(0.2, np.arange(2, 20, 2))                        ## 0.2 and 2 - 18 km/s (2 km/s step)
-            masses, metallicities, v_surf_init_list = get_grid(sample_masses, sample_metallicities, sample_v_init)   
-            coarse_age_list = 1E7 * np.ones(len(masses)).astype(float)               
+            sample_v_init = np.append(0.2, np.arange(2, 20, 2))        ## 0.2 and 2 - 18 km/s (2 km/s step)
+            masses, metallicities, v_surf_init_list = get_grid(sample_masses, sample_metallicities, sample_v_init)                
         else:
             ## Load grid
             arr = np.genfromtxt("./templates/coarse_age_map.csv",
                             delimiter=",", dtype=str, skip_header=1)
             masses = arr[:,0].astype(float)
             metallicities = arr[:,1].astype(float)
-            coarse_age_list = [age*1E6 if age != 0 else 20*1E6 for age in arr[:,2].astype(float)]
             v_surf_init_list = np.random.randint(1, 10, len(masses)).astype(float) * 30
-    return masses, metallicities, coarse_age_list, v_surf_init_list
+    return masses, metallicities, v_surf_init_list
 
 
 
 if __name__ == "__main__":
-    parallel = True
+    parallel = False
     if parallel:
         os.environ['OMP_NUM_THREADS'] = "8"     
         ## Uses 8 logical cores per evolution process, works best for a machine with 16 logical cores i.e. 2 parallel processes.
