3dcf0f450f08084359ecac3de73eaf1cc8bef03b
gautam-404 <gautam.anuj.1195@gmail.com>
2023-02-22 19:39:07 +0000

    update  setup

---
+++ setup.py
@@ -0,0 +1,3 @@
+from MESAcontroller import Installer
+
+Installer(version='22.11.1', parentDir="./", cleanAfter=True,)
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip install numpy
+pip install git+https://github.com/gautam-404/MESA-controller.git
+
+python setup.py
