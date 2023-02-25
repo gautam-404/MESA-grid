c9ab86434555ae713621a2975ff08a33b184947b
gautam-404 <gautam.anuj.1195@gmail.com>
2023-02-26 04:23:39 +1100

    update

--- job.sh
+++ job.sh
@@ -4,10 +4,10 @@
 #PBS -l mem=16GB
 #PBS -l jobfs=200GB
 #PBS -q normal
-#PBS -P a00
-#PBS -l walltime=01:00:00
-#PBS -l storage=gdata/a00+scratch/a00
+#PBS -P ht06
+#PBS -l walltime=00:10:00
+#PBS -l storage=scratch/ht06
 #PBS -l wd
   
-module load python3
-python3 grid.py $PBS_NCPUS > /g/data/a00/$USER/job_logs/$PBS_JOBID.log
+source ~/.bashrc
+~/.pyenv/shims/python3 grid.py $PBS_NCPUS > /scratch/ht06/$USER/workspace/MESA-grid/job_logs/$PBS_JOBID.log
