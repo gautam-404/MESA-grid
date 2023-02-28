88a4f7da870da093373141ca5c1fd137b9979ec3
gautam-404 <gautam.anuj.1195@gmail.com>
2023-02-28 16:46:39 +1100

    update

--- ray-cluster.sh
+++ ray-cluster.sh
@@ -9,11 +9,12 @@ cd $PBS_O_WORKDIR
 
 UHOME=/home/594/ag9272
 nodeDnsIps=`cat $PBS_NODEFILE | uniq`
-headNodeDnsIp=`uname -n`
-headNodeIp=`hostname -i`
-rayDashboardPort=8888
+hostNodeDnsIp=`uname -n`
+hostNodeIp=`hostname -i`
+# rayDashboardPort=7711
 rayPort=5711
-rayPassword='5241590000000000'
+thishostNport="${hostNodeIp}:${rayPort}"
+redisPassword=$(uuidgen)
 
 
 cat > $PBS_O_WORKDIR/setupRayWorkerNode.sh << 'EOF'
@@ -22,43 +23,44 @@ set -e
 ulimit -s unlimited
 cd $PBS_O_WORKDIR
 
-headNodeIp=${1}
-rayPort=${2}
-rayPassword=${3}
-UHOME=${4}
-hostIpNPort=$headNodeIp:$rayPort
+############ MESA environment variables ###############
+export MESASDK_ROOT=/scratch/ht06/ag9272/workspace/software/mesasdk
+source $MESASDK_ROOT/bin/mesasdk_init.sh
+export MESA_DIR=/scratch/ht06/ag9272/workspace/software/mesa-r22.11.1
+export OMP_NUM_THREADS=16      
+export GYRE_DIR=$MESA_DIR/gyre/gyre
+#######################################################
 
-module purge
-module load pbs
+thishostNport=${1}
+redisPassword=${2}
+UHOME=${3}
+
 source $UHOME/.pyenv/versions/3.11.2/envs/ray/bin/activate
 
-echo `uname -n`
-echo `hostname -i`
-echo `ray start --address=$hostIpNPort --redis-password=$rayPassword &`
+thisNodeIp=`hostname -i`
+echo `ray start --address=$thishostNport --num-cpus=48 --redis-password=$redisPassword --block`
 echo Done.
 EOF
 
 chmod +x $PBS_O_WORKDIR/setupRayWorkerNode.sh
 
 echo "Setting up Ray cluster......."
-for nodeDnsIp in $nodeDnsIps
+J=0
+for nodeDnsIp in `echo ${nodeDnsIps}`
 do
-        if [[ $nodeDnsIp == $headNodeDnsIp ]]
+        if [[ ${nodeDnsIp} == "${hostNodeDnsIp}" ]]
         then
                 echo -e "\nStarting ray cluster on head node..."
-                module purge
-                module load pbs
+                source $UHOME/.bashrc
                 source $UHOME/.pyenv/versions/3.11.2/envs/ray/bin/activate
-                ray start --head --port=$rayPort
+                ray start --head --num-cpus=48 --port=$rayPort
                 sleep 5
         else
-                echo -e "\nStarting ray cluster on worker node..."
-                pbs_tmrsh ${nodeDnsIp} $PBS_O_WORKDIR/setupRayWorkerNode.sh $headNodeIp $rayPort $rayPassword $UHOME &
+                echo -e "\nStarting ray cluster on worker node ${hostNodeDnsIp} at ${thishostNport}"
+                pbs_tmrsh ${nodeDnsIp} $PBS_O_WORKDIR/setupRayWorkerNode.sh ${thishostNport} $redisPassword $UHOME &
                 sleep 5
         fi
+        J=$((J+1))
 done
 
 rm $PBS_O_WORKDIR/setupRayWorkerNode.sh
-
-echo -e "\nRay dashboard connection string:"
-echo "ssh -L $rayDashboardPort:0.0.0.0:$rayDashboardPort $USER@gadi.nci.org.au"
