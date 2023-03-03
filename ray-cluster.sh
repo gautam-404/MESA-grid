d7c9db795c14a65679e4e928c460818592c24f86
gautam-404 <gautam.anuj.1195@gmail.com>
2023-03-03 15:17:54 +1000

    update, generic use

--- ray-cluster.sh
+++ ray-cluster.sh
@@ -7,7 +7,7 @@
 
 cd $PBS_O_WORKDIR
 
-UHOME=/home/594/ag9272
+UHOME=`eval echo "~$USER"`
 nodeDnsIps=`cat $PBS_NODEFILE | uniq`
 headNodeDnsIp=`uname -n`
 headNodeIp=`hostname -i`
@@ -23,18 +23,11 @@ set -e
 ulimit -s unlimited
 cd $PBS_O_WORKDIR
 
-############ MESA environment variables ###############
-export MESASDK_ROOT=/scratch/ht06/ag9272/workspace/software/mesasdk
-source $MESASDK_ROOT/bin/mesasdk_init.sh
-export MESA_DIR=/scratch/ht06/ag9272/workspace/software/mesa-r22.11.1
-export OMP_NUM_THREADS=16      
-export GYRE_DIR=$MESA_DIR/gyre/gyre
-#######################################################
-
 headNodeIpNport=${1}
 redisPassword=${2}
 UHOME=${3}
 
+source $UHOME/.bashrc
 source $UHOME/.pyenv/versions/3.11.2/envs/ray/bin/activate
 
 thisNodeIp=`hostname -i`
