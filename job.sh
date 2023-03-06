8e5fa5ee6dad1ccf45c92b2c5ebe904ad7f7bc76
gautam-404 <gautam.anuj.1195@gmail.com>
2023-03-07 08:11:54 +1000

    update

--- job.sh
+++ job.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/bash -l
  
 #PBS -N GYRE
 #PBS -P 0201
 #PBS -q default
-#PBS -l select=4:ncpus=128:mem=512GB
-#PBS -l walltime=24:00:00
-#PBS -o $PBS_O_WORKDIR/job.out
-#PBS -e $PBS_O_WORKDIR/job.err
-#PBS -M anuj.gautam@usq.edu.au
-#PBS -m abe
+#PBS -l select=4:ncpus=128:mem=256GB
+#PBS -l walltime=12:00:00
+# PBS -M anuj.gautam@usq.edu.au
+# PBS -m abe
 
 source ~/.bashrc
 cd $PBS_O_WORKDIR
