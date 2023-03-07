c40bb911c1dc9e4048c1c7b756e418fd7431d55f
gautam-404 <gautam.anuj.1195@gmail.com>
2023-03-08 05:25:39 +1000

    update

--- num_threads_comp.py
+++ num_threads_comp.py
@@ -8,13 +8,22 @@ from multiprocessing import Pool
 import helper
 from rich import print, progress
 import humanize
-import tracemalloc
 
-def evo(num_threads, dirw, n=0):
+
+def evo(num_threads, dirw, n=0, gyre=False):
     os.environ["OMP_NUM_THREADS"] = str(num_threads)
     v_surf_init = 2
-    initial_mass = 1.3
-    initial_z = 0.02
+    initial_mass = 1.32
+    mass = initial_mass
+    Zinit = 0.004
+    metallicity = Zinit
+    name = f"{dirw}/work_{n}"
+    proj = ProjectOps(name)     
+    proj.create(overwrite=True) 
+    star = MesaAccess(name)
+    star.load_HistoryColumns("./templates/history_columns.list")
+    star.load_ProfileColumns("./templates/profile_columns.list")
+
     rotation_init_params = {'change_rotation_flag': True,   ## False for rotation off until near zams
                             'new_rotation_flag': True,
                             'change_initial_rotation_flag': True,
@@ -24,25 +33,65 @@ def evo(num_threads, dirw, n=0):
                             'relax_surface_rotation_v' : True,
                             'num_steps_to_relax_rotation' : 100, ## Default value is 100
                             'set_uniform_am_nu_non_rot': True}
-    params = helper.phases_params(initial_mass, initial_z)["Pre-MS Evolution"]     
-    terminal_age = float(np.round(2500/initial_mass**2.5,1)*1.0E6)
-    proj = ProjectOps(f"{dirw}/test{n}")
-    proj.create(overwrite=True)
-    star = MesaAccess(f"{dirw}/test{n}")
-    star.load_InlistProject("./templates/inlist_template")
-    star.set(params, force=True)
-    star.set(rotation_init_params, force=True)
-    star.set("max_age", terminal_age, force=True)
-    star.set({"convergence_ignore_equL_residuals" : True}, force=True)
-    proj.make()
-    proj.run()
-    return 
+    convergence_helper = {"convergence_ignore_equL_residuals" : True}  
+
+    inlist_template = "./templates/inlist_template"
+    failed = True   ## Flag to check if the run failed, if it did, we retry with a different initial mass (M+dM)
+    retry = -1
+    dM = [0, 1e-3, -1e-3, 2e-3, -2e-3]
+    while retry<len(dM) and failed:
+        proj.clean()
+        proj.make(silent=True)
+        phases_params = helper.phases_params(initial_mass, Zinit)     
+        phases_names = phases_params.keys()
+        terminal_age = float(np.round(2500/initial_mass**2.5,1)*1.0E6)
+        phase_max_age = [1E6, 1E7, 4.0E7, terminal_age]         ## 1E7 is the age when we switch to a coarser timestep
+        for phase_name in phases_names:
+            try:
+                ## Run from inlist template by setting parameters for each phase
+                star.load_InlistProject(inlist_template)
+                print(phase_name)
+                star.set(phases_params[phase_name], force=True)
+                star.set('max_age', phase_max_age.pop(0), force=True)
+                if phase_name == "Pre-MS Evolution":
+                    ## Initiate rotation
+                    star.set(rotation_init_params, force=True)
+                    if retry>=0:
+                        star.set(convergence_helper, force=True)
+                    proj.run(logging=True, parallel=True)
+                else:
+                    proj.resume(logging=True, parallel=True)
+            except Exception as e:
+                failed = True
+                print(e)
+                break
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt
+            else:
+                failed = False
+        if failed:
+            retry += 1
+            initial_mass = mass + dM[retry]
+            with open(f"{name}/run.log", "a+") as f:
+                if retry == len(dM)-1:
+                    f.write(f"Max retries reached. Model skipped!\n")
+                    break
+                f.write(f"\nMass: {mass} MSun, Z: {metallicity}, v_init: {v_surf_init} km/s\n")
+                f.write(f"Failed at phase: {phase_name}\n")
+                f.write(f"Retrying with dM = {dM[retry]}\n")
+                f.write(f"New initial mass: {initial_mass}\n")
+    if not failed:
+        if gyre:   ## Optional, GYRE can berun separately using the run_gyre function    
+            os.environ['OMP_NUM_THREADS'] = '8'
+            ## Run GYRE on multiple profile files parallely
+            proj.runGyre(gyre_in="templates/gyre_rot_template_dipole.in", files='all', data_format="FGONG", 
+                                logging=False, parallel=True, n_cores=num_threads)
 
 
 def measure_parallel():
-    total = 64
-    ncpus = 64
-    num_threads_list = [1, 8, 16, 24, 32]
+    total = 1
+    ncpus = 128
+    num_threads_list = [1, 2, 4, 8, 16, 24, 32]
     for num_threads in num_threads_list:
         name = "test"
         if os.path.exists(name):
@@ -55,7 +104,6 @@ def measure_parallel():
         print("No. of parallel tracks:", min([n_processes, total]))
         args = zip(repeat(num_threads), repeat(name), range(1, total+1))
         p_start = time.time()
-        tracemalloc.start()
         with Pool(n_processes, initializer=helper.mute) as p:
             with progress.Progress(*helper.progress_columns()) as progressbar:
                 task = progressbar.add_task("[b i green]Running...", total=total)
@@ -63,12 +111,9 @@ def measure_parallel():
                 for result in p.istarmap(evo, args):
                     times.append(result)
                     progressbar.advance(task)
-        max_mem = tracemalloc.get_traced_memory()[1]
-        tracemalloc.stop()
         p_end = time.time()
         total_time = (p_end - p_start)  ## seconds
-        print(f"Time taken: {humanize.naturaltime(total_time)}")
-        print(f"Max memory used: {humanize.naturalsize(max_mem)}")
+        print(f"Time taken: {total_time/60:.2f} minutes")
         plt.bar(num_threads, total_time, label=str(num_threads))
     plt.legend(title="num threads")
     plt.xlabel("num threads")
